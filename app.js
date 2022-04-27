0515e920dae2757f2e8b56d465e2a784ac9779a2
KevinSingaye <75949789+KevinSingaye@users.noreply.github.com>
2022-04-27 17:21:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');;
+const bodyParser = require('body-parser')
+const app = express();
+
+const stuffRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://KevinSingaye:Mongodb936!@cluster0.t3dpx.mongodb.net/bddsauce?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+
+
+module.exports = app;
